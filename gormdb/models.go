2335a9014f3ee9691dcf5a59cefa50e9dd3643fd
JObog <jobogbaimhe@gmail.com>
2021-10-17 00:14:50 -0400

    created Task model

--- gormdb/models.go
+++ gormdb/models.go
@@ -1,1 +1,16 @@
 package gormdb
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Task struct {
+	gorm.Model
+	ID          uint
+	Title       string
+	Description string
+	Completed   bool
+	CreatedAt   time.Time
+}
