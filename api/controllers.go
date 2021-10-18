9c9f248773c3f55ed162b3fcadee6bb329287230
JObog <jobogbaimhe@gmail.com>
2021-10-17 21:13:36 -0400

    added route to update a task

--- api/controllers.go
+++ api/controllers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/J-Obog/pomoGOro/gormdb"
+	"github.com/gorilla/mux"
 )
 
 
@@ -28,6 +29,7 @@ func CreateNewTask(w http.ResponseWriter, r *http.Request) {
 func GetAllTasks(w http.ResponseWriter, r *http.Request) {
 	var tasks []Task
 
+	//get all tasks from db
 	db := gormdb.Connect()
 	db.Find(&tasks)
 
@@ -43,9 +45,26 @@ func GetAllTasks(w http.ResponseWriter, r *http.Request) {
 }	
 
 func RemoveTask(w http.ResponseWriter, r *http.Request) {
-	
+
 }
 
 func UpdateTask(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	var body map[string]interface{}
+	err := json.NewDecoder(r.Body).Decode(&body)
 	
+	if err != nil {
+		w.WriteHeader(500)
+		return 
+	}
+
+	var task Task
+
+	db := gormdb.Connect()
+	//update task with associated id
+	db.First(&task,id)
+	db.Model(&task).Updates(body)
+		
+	w.WriteHeader(200)	
+	w.Write([]byte("Task successfully updated"))
 }
