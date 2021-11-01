69ac44a17ab7a91cb852da24290662aaad5c89e1
JObog <jobogbaimhe@gmail.com>
2021-11-01 02:53:49 -0400

    moved middleware to main package

--- server/main.go
+++ server/main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/J-Obog/pomodoro/db"
-	"github.com/J-Obog/pomodoro/mware"
 	"github.com/J-Obog/pomodoro/tasksvc"
 	"github.com/gorilla/mux"
 )
@@ -17,12 +16,12 @@ func main() {
 
 	//create and configure main router
 	router := mux.NewRouter().StrictSlash(true)
-	router.Use(mware.ReqLogger)
+	router.Use(ReqLogger)
 
 	//configure api task routes
 	tasksvc.AddRoutes(router.PathPrefix("/api").Subrouter())
 	
 	//spin up server
 	log.Println("Server running on port 8000")
-	log.Fatal(http.ListenAndServe(":8000", mware.CORS(router))) //router wrapped with CORS middleware
+	log.Fatal(http.ListenAndServe(":8000", CORS(router))) //router wrapped with CORS middleware
 }
--- server/middleware.go
+++ server/middleware.go
@@ -1,4 +1,4 @@
-package mware
+package main
 
 import (
 	"log"
