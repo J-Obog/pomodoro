32a1f341c9c32459932bad043e66c98f3e738b72
JObog <jobogbaimhe@gmail.com>
2021-11-01 10:54:48 -0400

    implementing password hashing to user registration

--- server/authsvc/controller.go
+++ server/authsvc/controller.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/J-Obog/pomodoro/db"
 	"github.com/J-Obog/pomodoro/usersvc"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func LogUserIn(w http.ResponseWriter, r *http.Request) {
@@ -15,11 +16,16 @@ func LogUserIn(w http.ResponseWriter, r *http.Request) {
 func RegisterNewUser(w http.ResponseWriter, r *http.Request) {
 	var user usersvc.User
 
+
 	if e := json.NewDecoder(r.Body).Decode(&user); e != nil {
 		w.WriteHeader(500)
 		return
 	}
-
+	
+	if hash, e := bcrypt.GenerateFromPassword([]byte(user.Password), 10); e != nil {
+		user.Password = string(hash)
+	}
+	
 	if e := db.DB.Create(&user).Error; e != nil {
 		w.WriteHeader(500)
 		return
--- server/go.mod
+++ server/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	gorm.io/driver/postgres v1.2.1
 	gorm.io/gorm v1.22.0
 )
