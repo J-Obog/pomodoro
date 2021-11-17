c61324901bde181db501f5754e49efde6c849387
JObog <jobogbaimhe@gmail.com>
2021-11-16 20:12:21 -0500

    now using trash icon for delete button

--- client/src/components/task/Task.js
+++ client/src/components/task/Task.js
@@ -6,7 +6,7 @@ const Task = (props) => {
     const { access } = useContext(AuthContext);
     const [complete, setCompletion] = useState(props.completed); 
     const [title, setTitle] = useState(props.title);
-    const [editing, setEditing] = useState(false); 
+    //const [editing, setEditing] = useState(false); 
 
     const deleteTask = async () => {
         try {
@@ -48,8 +48,8 @@ const Task = (props) => {
             </div>
             <div>
                 <button onClick={deleteTask}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="#F43F5E">
-                        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#4B5563">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                     </svg>
                 </button>
             </div>
