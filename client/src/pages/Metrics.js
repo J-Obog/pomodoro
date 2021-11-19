b5d99314c9301c66723c55780660f165ba794744
JObog <jobogbaimhe@gmail.com>
2021-11-19 15:36:58 -0500

    fixed divide by zero error in metrics route

--- client/src/pages/Metrics.js
+++ client/src/pages/Metrics.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { MetricList } from '../components/metrics';
+import Chart from 'react-google-charts';
 
 const Metrics = () => {
     return (
         <div className="flex flex-col justify-center items-center py-5 px-10">
             <div className="w-2/4">
-                <div>{/* bar chart */}</div>
+                <div>
+                    <Chart
+                        className="w-full"
+                        height={'300px'}
+                        chartType="Bar"
+                        loader={<div>Loading Chart</div>}
+                        data={[
+                            ['', 'Task Completed'],
+                            ['2014', 1000],
+                        ]}
+                        options={{
+                            legend: { position: 'none' },
+                            chart: {
+                                title: 'Past Activity',
+                                subtitle: 'Stuff from the last week',
+                            },
+                        }}
+                    />
+                </div>
                 <div>
                     <MetricList />
                 </div>
--- server/controllers/user.go
+++ server/controllers/user.go
@@ -32,7 +32,9 @@ func GetUserMetrics(w http.ResponseWriter, r *http.Request) {
 	jti := apputils.GetTokenJTI(r)
 	var tasks []models.Task
 	var groups = map[string]int{}
-	tasks_completed := 0
+	totalTasks := len(tasks)
+	tasksCompleted := 0
+	taskCompletionRate := 0.0
 	groupDays := 7
 
 	if e := data.DB.Where(&models.Task{UserID: jti}).Find(&tasks).Error; e != nil {
@@ -42,9 +44,9 @@ func GetUserMetrics(w http.ResponseWriter, r *http.Request) {
 
 	for _, v := range tasks {
 		if(v.CompletedAt != nil) {
-			tasks_completed++
+			tasksCompleted++
 			
-			//logic for grouping task by day
+			//logic for grouping tasks by day
 			b := time.Now().Add(time.Duration(-24 * groupDays) * time.Hour)
 			if(v.CompletedAt.Equal(b) || v.CompletedAt.After(b)) {
 				k := v.CompletedAt.Format("Jan-02-2006")
@@ -57,11 +59,15 @@ func GetUserMetrics(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	if(totalTasks != 0) {
+		taskCompletionRate = float64(tasksCompleted) / float64(totalTasks)
+	}
+
 	json.NewEncoder(w).Encode(map[string]interface{}{
 		"metrics": []map[string]interface{}{
-			{"metric": "Total Tasks Created", "value": len(tasks)},
-			{"metric": "Total Tasks Completed", "value": tasks_completed},
-			{"metric": "Task Completion Rate", "value": fmt.Sprintf("%f%%", (float64(tasks_completed) / float64(len(tasks))) * 100)}, 
+			{"metric": "Total Tasks Created", "value": totalTasks},
+			{"metric": "Total Tasks Completed", "value": tasksCompleted},
+			{"metric": "Task Completion Rate", "value": fmt.Sprintf("%f%%", taskCompletionRate)}, 
 		},
 		"tasks_by_day": groups, 
 	})
